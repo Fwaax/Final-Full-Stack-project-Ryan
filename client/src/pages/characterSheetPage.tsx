181671180f42e3d62230bc2fbcbd76fd2869aa0c
Ryan <ryan32111@walla.co.il>
2024-09-06 18:41:22 +0300

    Stats

--- client/src/pages/characterSheetPage.tsx
+++ client/src/pages/characterSheetPage.tsx
@@ -11,11 +11,7 @@ const CharacterSheetPage = () => {
 
 
 
-                <div className='bg-green-500 h-[100px]'>
-                    <div>
-
-                    </div>
-                </div>
+                <StatsPanel />
 
 
                 <div className='bg-red-700 h-[800px]'></div>
@@ -28,21 +24,21 @@ function TopPanel() {
     return <div className=' h-[100px] flex flex-row justify-between items-center'>
         <div className=' w-[250px] h-full flex flex-row gap-2'>
             <div className='flex flex-row justify-center items-center'>
-                <img src="./img/1949499.jpg" alt="img" className='w-[100px] border border-solid border-black' />
+                <img src="./img/1949499.jpg" alt="img" className='w-[100px] border border-solid border-red-700' />
             </div>
             <div>
                 <div className='flex flex-row'>
-                    <h6 className='text-sm'>Character Name</h6> <button className='text-sm bg-gray-700 border border-solid border-black rounded-sm text-white'>MANAGE</button>
+                    <h6 className='text-sm text-white'>Character Name</h6> <button className='text-sm border border-solid border-red-700 hover:border-red-600 rounded-sm text-white'>MANAGE</button>
                 </div>
-                <p className='text-sm'>Character Level and Race</p>
+                <p className='text-sm text-white'>Character Level and Race</p>
             </div>
         </div>
         <div className=' w-[300px] h-full flex flex-row justify-center items-center gap-3'>
-            <div className='flex flex-row justify-center items-center border border-red-700 border-solid hover:border-red-600 cursor-pointer px-1'>
+            <div className='flex flex-row justify-center items-center border border-red-700 border-solid rounded-sm hover:border-red-600 cursor-pointer px-1'>
                 <CampfireSvg className="w-[25px]" />
                 <h6 className='text-white'>SHORT REST</h6>
             </div>
-            <div className='flex flex-row justify-center items-center border border-red-700 border-solid hover:border-red-600 cursor-pointer px-1'>
+            <div className='flex flex-row justify-center items-center border border-red-700 border-solid rounded-sm hover:border-red-600 cursor-pointer px-1'>
                 <MoonSvg className="w-[25px] h-[15px]" />
                 <h6 className='text-white'>LONG REST</h6>
             </div>
@@ -50,6 +46,47 @@ function TopPanel() {
                 <AnvilSvg className="w-[25px]" />
             </div>
         </div>
+    </div>
+}
+
+const stats = [
+    {
+        name: "STR",
+        modifier: 0,
+        currentStat: 0
+    },
+    {
+        name: "DEX",
+        modifier: 0,
+        currentStat: 0
+    },
+    {
+        name: "CON",
+        modifier: 0,
+        currentStat: 0
+    }
+]
+function StatsPanel() {
+    return <div className='h-[100px] flex flex-row gap-1 items-center w-full'>
+        {stats.map((stat) => {
+            return <CoreStatCard name={stat.name} key={stat.name} modifier={stat.modifier} currentStat={stat.currentStat} />
+        })}
+
+    </div>
+}
+
+interface CoreStatCardProps {
+    name: string;
+    modifier: number;
+    currentStat: number;
+}
+function CoreStatCard(props: CoreStatCardProps) {
+    return <div className='flex flex-col items-center justify-center border border-red-700 border-solid w-fit p-2'>
+        <h6>{props.name}</h6>
+        <div>
+            <h6>+{props.modifier}</h6>
+        </div>
+        <h6>{props.currentStat}</h6>
     </div>
 }
 
